a742f372d847a1e1e3d75a6641a1fb3dcfbd7af9
Márcio Rodrigues Filho <rodriguesfilhomarcio@gmail.com>
2021-05-24 16:20:51 -0300

    Atribuiçao de operaçoes - Done

--- mvcbpl.c
+++ mvcbpl.c
@@ -18,12 +18,12 @@ unsigned int int_stack_addr(struct LocalVariable *localVariables, char c1, int d
     }
     else // if (c1 == 'v')
     {
-        return localVariables[d-1].vAddr;
+        return localVariables[d - 1].vAddr;
     }
 }
 
 void process_1st_parameter(struct LocalVariable *localVariables,
-    char c1, char c2, char d)
+                           char c1, char c2, char d)
 {
     if (c1 == 'c')
     {
@@ -42,7 +42,7 @@ void process_1st_parameter(struct LocalVariable *localVariables,
 }
 
 void process_2nd_parameter(struct LocalVariable *localVariables,
-    char c1, char c2, char d)
+                           char c1, char c2, char d)
 {
     if (c1 == 'c')
     {
@@ -61,7 +61,7 @@ void process_2nd_parameter(struct LocalVariable *localVariables,
 }
 
 void process_3rd_parameter(struct LocalVariable *localVariables,
-    char c1, char c2, char d)
+                           char c1, char c2, char d)
 {
     if (c1 == 'c')
     {
@@ -114,22 +114,106 @@ void compile_assignment_command(char *line, struct LocalVariable *localVariables
 {
     // TODO: [x]simple assignment, [ ]expression or [x]function return
     int r, a, b, d1, d2, d3;
-    char c11, c12, c21, c22, c31, c32;
+    char c11, c12, c21, c22, c31, c32, operacao;
 
     printf("\n\t# %s", line);
+
+    // expression assignment
+    r = sscanf(line, "vi%d = %ci%d %c %ci%d", &a, &c11, &d1, &operacao, &c21, &d2);
+    if (r == 6)
+    {
+        if (c11 == 'c')
+        {
+            printf("\tmovl $%d, %%eax\n", d1);
+        }
+        else if (c11 == 'v')
+        {
+            printf("\tmovl -%u(%%rbp), %%eax\n", localVariables[d1 - 1].vAddr);
+        }
+        else
+        {
+            if (d1 == 1)
+            {
+                printf("\tmovl %%edi, %%eax\n");
+            }
+            else if (d1 == 2)
+            {
+                printf("\tmovl %%esi, %%eax\n");
+            }
+            else
+            {
+                printf("\tmovl %%edx, %%eax\n");
+            }
+        }
+
+        if (c21 == 'c')
+        {
+            printf("\tmovl $%d, %%ecx\n", d2);
+        }
+        else if (c21 == 'v')
+        {
+            printf("\tmovl -%u(%%rbp), %%ecx\n", localVariables[d2 - 1].vAddr);
+        }
+        else
+        {
+            if (d2 == 1)
+            {
+                printf("\tmovl %%edi, %%ecx\n");
+            }
+            else if (d2 == 2)
+            {
+                printf("\tmovl %%esi, %%ecx\n");
+            }
+            else
+            {
+                printf("\tmovl %%edx, %%ecx\n");
+            }
+        }
+
+        switch (operacao)
+        {
+        case '+':
+            printf("\taddl %%ecx, %%eax\n");
+            break;
+        case '-':
+            printf("\tsubl %%ecx, %%eax\n");
+            break;
+        case '*':
+            printf("\timull %%ecx, %%eax\n");
+            break;
+        case '/':
+            if (parametersCount == 3)
+            {
+                printf("\tmovl %%edx, %%r8d\n");
+                printf("\tcltd\n");
+                printf("\tidivl %%ecx\n");
+                printf("\tmovl %%r8d, %%edx\n");
+            }
+            else
+            {
+                printf("\tcltd\n");
+                printf("\tidivl %%ecx\n");
+            }
+
+            break;
+        }
+
+        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a - 1].vAddr);
+        return;
+    }
 
     // simple assignment
     r = sscanf(line, "vi%d = ci%d", &a, &b);
     if (r == 2)
     {
-        printf("\tmovl $%d, -%u(%%rbp)\n", b, localVariables[a-1].vAddr);
+        printf("\tmovl $%d, -%u(%%rbp)\n", b, localVariables[a - 1].vAddr);
         return;
     }
     r = sscanf(line, "vi%d = vi%d", &a, &b);
     if (r == 2)
     {
-        printf("\tmovl -%u(%%rbp), %%eax\n", localVariables[b-1].vAddr);
-        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a-1].vAddr);
+        printf("\tmovl -%u(%%rbp), %%eax\n", localVariables[b - 1].vAddr);
+        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a - 1].vAddr);
         return;
     }
     r = sscanf(line, "vi%d = pi%d", &a, &b);
@@ -137,31 +221,32 @@ void compile_assignment_command(char *line, struct LocalVariable *localVariables
     {
         if (b == 1)
         {
-            printf("\tmovl %%edi, -%u(%%rbp)\n", localVariables[a-1].vAddr);
+            printf("\tmovl %%edi, -%u(%%rbp)\n", localVariables[a - 1].vAddr);
         }
         else if (b == 2)
         {
-            printf("\tmovl %%esi, -%u(%%rbp)\n", localVariables[a-1].vAddr);
+            printf("\tmovl %%esi, -%u(%%rbp)\n", localVariables[a - 1].vAddr);
         }
         else // if (b == 3)
         {
-            printf("\tmovl %%edx, -%u(%%rbp)\n", localVariables[a-1].vAddr);
+            printf("\tmovl %%edx, -%u(%%rbp)\n", localVariables[a - 1].vAddr);
         }
         return;
     }
 
+    //
     // function return assignment
     r = sscanf(line, "vi%d = call f%d %c%c%d %c%c%d %c%c%d", &a, &b,
-        &c11, &c12, &d1, &c21, &c22, &d2, &c31, &c32, &d3);
+               &c11, &c12, &d1, &c21, &c22, &d2, &c31, &c32, &d3);
     if (r == 2)
     {
         printf("\tcall f%d\n", b);
-        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a-1].vAddr);
+        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a - 1].vAddr);
         return;
     }
     else if (r == 5)
     {
-        if (parametersCount > 0)  // save %rdi in the stack
+        if (parametersCount > 0) // save %rdi in the stack
         {
             printf("\tmovq %%rdi, -8(%%rbp)\n");
         }
@@ -169,9 +254,9 @@ void compile_assignment_command(char *line, struct LocalVariable *localVariables
         process_1st_parameter(localVariables, c11, c12, d1);
 
         printf("\tcall f%d\n", b);
-        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a-1].vAddr);
+        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a - 1].vAddr);
 
-        if (parametersCount > 0)  // get %rdi back from the the stack
+        if (parametersCount > 0) // get %rdi back from the the stack
         {
             printf("\tmovq -8(%%rbp), %%rdi\n");
         }
@@ -179,11 +264,11 @@ void compile_assignment_command(char *line, struct LocalVariable *localVariables
     }
     else if (r == 8)
     {
-        if (parametersCount > 0)  // save %rdi in the stack
+        if (parametersCount > 0) // save %rdi in the stack
         {
             printf("\tmovq %%rdi, -8(%%rbp)\n");
         }
-        if (parametersCount > 1)  // save %rsi in the stack
+        if (parametersCount > 1) // save %rsi in the stack
         {
             printf("\tmovq %%rsi, -16(%%rbp)\n");
         }
@@ -192,13 +277,13 @@ void compile_assignment_command(char *line, struct LocalVariable *localVariables
         process_2nd_parameter(localVariables, c21, c22, d2);
 
         printf("\tcall f%d\n", b);
-        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a-1].vAddr);
+        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a - 1].vAddr);
 
-        if (parametersCount > 0)  // get %rdi back from the the stack
+        if (parametersCount > 0) // get %rdi back from the the stack
         {
             printf("\tmovq -8(%%rbp), %%rdi\n");
         }
-        if (parametersCount > 1)  // get %rsi back from the the stack
+        if (parametersCount > 1) // get %rsi back from the the stack
         {
             printf("\tmovq -16(%%rbp), %%rsi\n");
         }
@@ -206,15 +291,15 @@ void compile_assignment_command(char *line, struct LocalVariable *localVariables
     }
     else // if (r == 11)
     {
-        if (parametersCount > 0)  // save %rdi in the stack
+        if (parametersCount > 0) // save %rdi in the stack
         {
             printf("\tmovq %%rdi, -8(%%rbp)\n");
         }
-        if (parametersCount > 1)  // save %rsi in the stack
+        if (parametersCount > 1) // save %rsi in the stack
         {
             printf("\tmovq %%rsi, -16(%%rbp)\n");
         }
-        if (parametersCount > 2)  // save %rdx in the stack
+        if (parametersCount > 2) // save %rdx in the stack
         {
             printf("\tmovq %%rdx, -24(%%rbp)\n");
         }
@@ -224,24 +309,22 @@ void compile_assignment_command(char *line, struct LocalVariable *localVariables
         process_3rd_parameter(localVariables, c32, c32, d3);
 
         printf("\tcall f%d\n", b);
-        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a-1].vAddr);
+        printf("\tmovl %%eax, -%u(%%rbp)\n", localVariables[a - 1].vAddr);
 
-        if (parametersCount > 0)  // get %rdi back from the the stack
+        if (parametersCount > 0) // get %rdi back from the the stack
         {
             printf("\tmovq -8(%%rbp), %%rdi\n");
         }
-        if (parametersCount > 1)  // get %rsi back from the the stack
+        if (parametersCount > 1) // get %rsi back from the the stack
         {
             printf("\tmovq -16(%%rbp), %%rsi\n");
         }
-        if (parametersCount > 2)  // get %rdx back from the the stack
+        if (parametersCount > 2) // get %rdx back from the the stack
         {
             printf("\tmovq -24(%%rbp), %%rdx\n");
         }
         return;
     }
-
-    // expression assignment
 }
 
 void compile_return_command(char *line, struct LocalVariable *localVariables)
@@ -355,7 +438,8 @@ void compile_function(int parametersCount, int functionIdentifier)
     {
         // assignment
         r = sscanf(line, "vi%d", &a);
-        if (r == 1) {
+        if (r == 1)
+        {
             compile_assignment_command(line, localVariables, parametersCount);
         }
 
@@ -394,7 +478,8 @@ int main()
     {
         r = sscanf(line, "function f%d p%c1, p%c2, p%c3", &functionIdentifier, &p1, &p2, &p3);
 
-        if (r > 0) {
+        if (r > 0)
+        {
             printf(".globl f%d\nf%d:\n", functionIdentifier, functionIdentifier);
             compile_function(r - 1, functionIdentifier);
             printf("\n");
